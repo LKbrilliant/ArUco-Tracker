6b8719b1b9d38c789f5f08e5ed8c6659b2470d5b
lkbrilliant <gunawardhana.mpav@gmail.com>
2022-11-01 19:36:13 +0800

    solve integram windup and derivative kick

--- PID.py
+++ PID.py
@@ -2,35 +2,6 @@
 # coding: utf-8
 #  PID control
 # *****************************************************************#
-#                   Incremental PID system                           #
-# *****************************************************************#
-class IncrementalPID:
-    def __init__(self, P, I, D):
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
-        self.PIDOutput = 0.0  
-        self.SystemOutput = 0.0  
-        self.LastSystemOutput = 0.0  
-        self.Error = 0.0 
-        self.LastError = 0.0
-        self.LastLastError = 0.0
-
-    # Set PID controller parameters
-    def SetStepSignal(self, StepSignal):
-        self.Error = StepSignal - self.SystemOutput
-        IncrementValue = self.Kp * (self.Error - self.LastError) + \
-                         self.Ki * self.Error + \
-                         self.Kd * (self.Error - 2 * self.LastError + self.LastLastError)
-        self.PIDOutput += IncrementValue
-        self.LastLastError = self.LastError
-        self.LastError = self.Error
-    # Set InertiaTime as inertial time constant in the first-order inertial link system
-    def SetInertiaTime(self, InertiaTime, SampleTime):
-        self.SystemOutput = (InertiaTime * self.LastSystemOutput + \
-                             SampleTime * self.PIDOutput) / (SampleTime + InertiaTime)
-        self.LastSystemOutput = self.SystemOutput
-# *****************************************************************#
 #                      Position type PID system                   #
 # *****************************************************************#
 class PositionalPID:
@@ -41,17 +12,32 @@ class PositionalPID:
         self.SystemOutput = 0.0
         self.ResultValueBack = 0.0
         self.PidOutput = 0.0
-        self.PIDErrADD = 0.0
-        self.ErrBack = 0.0
+        self.Iterm = 0.0
+        self.preErr = 0.0
+        self.outMin = -400 # 2-axis
+        self.outMax = 400  # 2-axis
     # Set PID controller parameters
     def SetStepSignal(self, StepSignal):
+		# Clamped output and Integral term
         Err = StepSignal - self.SystemOutput
         KpWork = self.Kp * Err
-        KiWork = self.Ki * self.PIDErrADD
-        KdWork = self.Kd * (Err - self.ErrBack)
-        self.PidOutput = KpWork + KiWork + KdWork
-        self.PIDErrADD += Err
-        self.ErrBack = Err
+        self.Iterm += self.Ki * Err
+        #KdWork = self.Kd * (Err - self.preErr)
+        KdWork = self.Kd * (self.SystemOutput - self.preErr)
+
+        if (self.Iterm > self.outMax): self.Iterm = self.outMax
+        elif(self.Iterm < self.outMin): self.Iterm= self.outMin
+
+        #self.PidOutput = KpWork + self.Iterm + KdWork
+        self.PidOutput = KpWork + self.Iterm - KdWork # subtract Kdterm to remove derivative kick
+
+        if(self.PidOutput > self.outMax): self.PidOutput = self.outMax;
+        elif(self.PidOutput < self.outMin): self.PidOutput = self.outMin;
+
+        #self.preErr = Err
+        self.preErr = self.SystemOutput
+        #print(f'P={KpWork:4.1f}, I{self.Iterm:4.1f}, D{KdWork:4.1f}, out{self.PidOutput:4.1f}, err{Err:4.1f}')
+
     # Set InertiaTime as inertial time constant in the first-order inertial link system
     def SetInertiaTime(self, InertiaTime, SampleTime):
         self.SystemOutput = (InertiaTime * self.ResultValueBack + \
